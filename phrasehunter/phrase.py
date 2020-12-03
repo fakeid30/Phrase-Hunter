8025823943547b932f56923e5a8c2da23c1120ba
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2020-12-03 21:52:18 +0600

    Removed f-string

--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -7,7 +7,7 @@ class Phrase:
             if letter in guesses:
                 print(f"{letter}", end=" ")
             else:
-                print(f"_", end=" ")
+                print("_", end=" ")
 
     def check_guess(self, guess):
         let = []
