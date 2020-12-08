2bba87bd6d4dbc6476630488dd9db4fdbc0b9d60
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2020-12-08 14:58:51 +0600

    Bugfix

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -6,12 +6,11 @@ import sys
 class Game:
     def __init__(self):
         self.missed = 0
-        self.phrases = []
+        self.phrases = self.create_phrases()
         self.guesses = [" "]
-        self.create_phrases(self.phrases)
         self.active_phrase = self.get_random_phrase()
 
-    def create_phrases(self, phrases):
+    def create_phrases(self):
         list_of_phrases = [
             "Hello World",
             "There is no Trying",
@@ -51,31 +50,40 @@ class Game:
                     self.active_phrase.display(self.guesses)
                     user_guess = input("\nEnter a Letter: ")
                     user_guess = user_guess.lower()
+
                     if not re.match("^[A-Za-z]*$", user_guess):
                         print("\nOnly (A-Z) and (a-z) Allowed")
                         replay = input("\nDo you want to replay?\n")
                         self.error_replay(replay)
+
                     elif len(user_guess) > 1:
                         print("\nOnly one character allowed")
                         replay = input("\nDo you want to replay?\n")
                         self.error_replay(replay)
                     self.get_guess(user_guess)
+
                     if not self.active_phrase.check_guess(user_guess):
                         self.remove_guess()
                         self.missed += 1
                 elif self.active_phrase.check_complete(self.guesses):
+
                     print("\nCongratulations you have won the game.\n")
                     ask = input("\nDo you want to replay this game?\n")
                     ask = ask.lower()
+
                     if ask == "yes":
                         again_ask = input(
                             "\nDo you want to play this game in a fresh state?\n"
                         )
+
                         again_ask = again_ask.lower()
+
                         if again_ask == "yes":
                             self.guesses = [" "]
                             self.missed = 0
+                            self.active_phrase = self.get_random_phrase()
                             self.start()
+
                         elif again_ask == "no":
                             self.guesses = [" "]
                             self.start()
@@ -98,8 +106,8 @@ class Game:
     def remove_guess(self):
         self.guesses.pop()
 
-    def error_replay(self, input):
-        input = input.lower()
+    def error_replay(self, _input):
+        _input = _input.lower()
         if input == "yes":
             self.guesses = [" "]
             self.missed = 0
@@ -107,8 +115,8 @@ class Game:
         else:
             sys.exit()
 
-    def game_reset_zero(self, input):
-        input = input.lower()
+    def game_reset_zero(self, _input):
+        _input = _input.lower()
         if input == "yes":
             self.guesses = [" "]
             self.missed = 0
